4b11afed7dde250080c729d51f00abef1fb41b67
0xtr <oxtrr@protonmail.com>
2023-04-15 23:25:40 +0200

    Add tokio tracing and pubkey blacklist

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::net::SocketAddr;
 
 use bb8_postgres::PostgresConnectionManager;
-use log::{debug, error, info};
 use nauthz_grpc::authorization_server::{Authorization, AuthorizationServer};
-use nauthz_grpc::{Decision, EventReply, EventRequest, Event};
+use nauthz_grpc::{Decision, Event, EventReply, EventRequest};
 use tokio_postgres::NoTls;
 use tonic::{transport::Server, Request, Response, Status};
+use tracing::{debug, info};
 
 pub struct EventAuthz {
     connection_pool: bb8::Pool<PostgresConnectionManager<NoTls>>,
@@ -22,22 +22,46 @@ impl Authorization for EventAuthz {
         request: Request<EventRequest>,
     ) -> Result<Response<EventReply>, Status> {
         let req = request.into_inner();
-        let event = req.clone().event.unwrap();
+        let event: Event = req.clone().event.unwrap();
         let content_sample: String = event.content.chars().take(40).collect();
 
-        let author = match req.auth_pubkey {
-            Some(_) => req.auth_pubkey(),
-            None => &event.pubkey,
-        };
+        let author = hex::encode(event.pubkey);
+        
+        info!(
+            "recvd event, [kind={}, origin={:?}, author={:?}, tag_count={}, content_sample={:?}]",
+            event.kind,
+            req.origin,
+            author,
+            event.tags.len(),
+            content_sample
+        );
 
-        let author = hex::encode(author);
+        // Fetch DB connection from connection pool
+        let conn = self.connection_pool.get().await.unwrap();
 
-        debug!("recvd event, [kind={}, origin={:?}, author={:?}, tag_count={}, content_sample={:?}]",
-                    event.kind, req.origin, author, event.tags.len(), content_sample);
+        // ================================
+        // Check if pubkey is blacklisted
+        // ================================
+        debug!("checking if publick key is blacklisted [pubkey={}]", author);
+        let author_blacklisted = conn
+            .query(
+                "SELECT EXISTS(SELECT 1 from blacklisted_pubkeys WHERE pubkey = $1)",
+                &[&author],
+            )
+            .await
+            .unwrap();
+        if author_blacklisted[0].get(0) {
+            info!("event denied: blacklisted pubkey [pubkey={}]", author);
+            return Ok(Response::new(nauthz_grpc::EventReply {
+                decision: Decision::Deny as i32,
+                message: Some("Event denied".to_string()),
+            }));
+        }
 
-        let mut conn = self.connection_pool.get().await.unwrap();
-
-        let is_blacklisted = conn
+        // ===============================================
+        // Check if content contains blacklisted content 
+        // ===============================================
+        let contains_blacklisted_content = conn
             .query(
                 "SELECT EXISTS(SELECT 1 FROM blacklisted_words WHERE word = $1)",
                 &[&event.content],
@@ -45,20 +69,21 @@ impl Authorization for EventAuthz {
             .await
             .unwrap();
 
-        let is_blacklisted: bool = is_blacklisted[0].get(0);
-
-        if is_blacklisted {
-            info!("event denied, content=[{:?}]", content_sample);
-            Ok(Response::new(nauthz_grpc::EventReply {
+        if contains_blacklisted_content[0].get(0) {
+            info!("event denied, blacklisted content [content={:?}]", content_sample);
+            return Ok(Response::new(nauthz_grpc::EventReply {
                 decision: Decision::Deny as i32,
                 message: Some("Event denied".to_string()),
-            }))
-        } else {
-            return Ok(Response::new(nauthz_grpc::EventReply {
-                decision: Decision::Permit as i32,
-                message: Some("Ok".to_string()),
             }));
         }
+
+        // None of the previous checks triggered, permit event
+        debug!("event permitted");
+        
+        return Ok(Response::new(nauthz_grpc::EventReply {
+            decision: Decision::Permit as i32,
+            message: Some("Ok".to_string()),
+        }));
     }
 }
 
@@ -78,8 +103,10 @@ async fn create_pool() -> bb8::Pool<PostgresConnectionManager<NoTls>> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Initialize logger
-    pretty_env_logger::init();
+    // construct a subscriber that prints formatted traces to stdout
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    // use that subscriber to process traces emitted after this point
+    tracing::subscriber::set_global_default(subscriber)?;
 
     let addr: SocketAddr = "[::1]:50051".parse().unwrap();
 
